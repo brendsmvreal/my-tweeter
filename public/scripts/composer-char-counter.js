9739b99c0696143440ba101a2d5e8532593f3b91
Brenda <brendamvreal@gmail.com>
2022-04-02 19:46:56 -0400

    Lint file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $(".textarea").keyup(function () {
+  $(".textarea").keyup(function() {
     const textToVal = $(this).val();
     let maxLen = 140 - textToVal.length;
     let counter = $(this).parent().find(".counter");
-    counter.val(maxLen)
+    counter.val(maxLen);
     if (maxLen < 0) {
-      counter.css('color', 'red') 
+      counter.css('color', 'red');
     } else {
       counter.css('color', '');
     }
